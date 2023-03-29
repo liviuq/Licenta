9ae60ed684b8d1be1874c713e60fadb5dbb6f841
Andrei Petrache <petrache.andrei1@gmail.com>
2023-03-29 18:33:07 +0300

    Added the code for the NRF24L01+ module

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -1,13 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import struct
+from threading import Thread
+from pyrf24 import RF24, RF24Network, RF24NetworkHeader
 
+from .update_db import update_database
 
 # creating the database
 db = SQLAlchemy()
 
 # setting the db name
 DB_NAME = 'database.db'
+
+# creating the radio object
+radio = RF24(22, 0)
+
+# creating the network on the radio object
+network = RF24Network(radio)
+
+# Address of our node in Octal format (01, 021, etc)
+THIS_NODE = 0o0  # make this node behave like the network master node
+
+# initialize the nRF24L01 on the spi bus
+if not radio.begin():
+    raise OSError("nRF24L01 hardware isn't responding")
+
+radio.channel = 90
+network.begin(THIS_NODE)
+
+EXPECTED_SIZE = struct.calcsize("<BL")
+
+# create the thread to update the database
+update_db_thread = Thread(update_database())
 
 
 def create_app():
@@ -32,6 +57,9 @@ def create_app():
     from .models import Sensor
     create_db(app)
 
+    # constantly update the database with new values
+    update_db_thread.start()
+    
     # return the flask app
     return app
 
