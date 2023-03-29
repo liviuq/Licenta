4efee881b1a2ca2fcf486e09ae9cd8e300c03737
Andrei Petrache <petrache.andrei1@gmail.com>
2023-03-29 18:32:41 +0300

    Updating the database as information is received by the base station

---
+++ flask/application/update_db.py
@@ -0,0 +1,27 @@
+import struct
+
+from . import db, network, radio, EXPECTED_SIZE
+from .models import Sensor
+
+def update_database():
+    try:
+        while True:
+            network.update()
+            while network.available():
+                header, payload = network.read()
+                sensor_type, value = struct.unpack("<BL", payload[:EXPECTED_SIZE])
+                sensor_type = chr(sensor_type)
+                address = header.to_string().split(' ')[3]
+                print(f'payload len: {len(payload)}, sensor type: {sensor_type}, value: {value}, header: {header.to_string()}')
+
+                # creating a db entry
+                sensor_db_entry = Sensor(address=address, type=sensor_type, value=value)
+
+                # add to database
+                db.session.add(sensor_db_entry)
+
+                # commit the changes to the db
+                db.session.commit()
+    except KeyboardInterrupt:
+        print("powering down radio and exiting.")
+        radio.power = False
