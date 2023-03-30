2f7e954bba4d4158b2d35716ee79bfd1ba0e6085
Andrei Petrache <petrache.andrei1@gmail.com>
2023-03-30 20:30:20 +0300

    Added route to get all sensor values

--- flask/application/views.py
+++ flask/application/views.py
@@ -5,8 +5,23 @@ from .models import Sensor
 # creating a blueprint
 views = Blueprint('views', __name__)
 
+@views.route('/latest', methods=['GET'])
+def get_latest():
+    all_data = Sensor.query.order_by(Sensor.id.desc()).all()
+    data_list = []
+    for entry in all_data:
+        data_dict = {
+            'id': entry.id,
+            'address': entry.address,
+            'type': entry.type,
+            'value': entry.value,
+            'date': entry.date
+        }
+        data_list.append(data_dict)
+    return jsonify(data_list)
+
 @views.route('/latest/<int:number>', methods=['GET'])
-def get_latest(number):
+def get_latest_number_entries(number):
     all_data = Sensor.query.order_by(Sensor.id.desc()).limit(number).all()
     data_list = []
     for entry in all_data:
