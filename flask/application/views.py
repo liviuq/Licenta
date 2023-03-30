558043393b5c6f6acead01f05ffa239e4e0f71fb
Andrei Petrache <petrache.andrei1@gmail.com>
2023-03-30 19:22:53 +0300

    Db working wit app_context

--- flask/application/views.py
+++ flask/application/views.py
@@ -1,12 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+from .models import Sensor
 
 # creating a blueprint
 views = Blueprint('views', __name__)
 
-@views.route('/latest')
-def latest():
-    return '<p>Latest values</p>'
+@views.route('/latest', methods=['GET'])
+def get_latest():
+    all_data = Sensor.query.all()
+    data_list = []
+    for entry in all_data:
+        data_dict = {
+            'id': entry.id,
+            'address': entry.address,
+            'type': entry.type,
+            'value': entry.value,
+            'date': entry.date
+        }
+        data_list.append(data_dict)
+    return jsonify(data_list)
 
 @views.route('/latest/10')
 def latest_10():
--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,12 @@
-from application import create_app
+from threading import Thread
 
-app = create_app()
+from application import create_app
+from application.update_db import update_database
+
+app, db = create_app()
 
 if __name__ == '__main__':
+    update_db = Thread(target=update_database, args=(app, db,))
+    update_db.start()
     app.run(debug=True, host='0.0.0.0')
 
