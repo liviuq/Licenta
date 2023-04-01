1b3c47be42221305110c5e4e93fa60ebd8831a0b
liviuq <petrache.andrei1@gmail.com>
2023-04-01 12:03:21 +0100

    Cannot run with commandline argument --debug

--- flask/main.py
+++ flask/main.py
@@ -3,13 +3,15 @@ from threading import Thread
 from application import create_app
 from application.update_db import update_database
 
+# creating the flask app which returns the app object and db instance
 app, db = create_app()
 
+# run the app only if called from app.py
 if __name__ == '__main__':
+    # spin up a thread to constantly populate the database with new sensor data
     update_db = Thread(target=update_database, args=(app, db,))
     update_db.start()
 
-    # remove the debug=True and run with
-    # debug in console: --debug
+    # run flask app
     app.run(debug=True, host='0.0.0.0')
 
