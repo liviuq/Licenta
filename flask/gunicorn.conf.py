1bfc0de267195fe20d01ae87b704abb674f6e513
liviuq <petrache.andrei1@gmail.com>
2023-04-03 19:01:46 +0100

    Added HTTPS support using a self-signed certificate

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -3,3 +3,7 @@ bind = '0.0.0.0:8000'
 accesslog = '-'
 errorlog = '-'
 loglevel = 'debug'
+
+# self-signed ssl certificate path
+certfile = '/home/andrew/ssl/cert.pem'
+keyfile = '/home/andrew/ssl/key.pem'
