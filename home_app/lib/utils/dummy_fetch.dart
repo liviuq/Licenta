a655026967727a4bea35cf9a03b5f2d509ec5d3f
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-04-30 16:54:46 +0300

    added: Function to retrieve sensor categories

--- home_app/lib/utils/dummy_fetch.dart
+++ home_app/lib/utils/dummy_fetch.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+
 import '../models/sensor.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,6 +15,27 @@ Future<Sensor> fetchData() async {
     // ignore: avoid_print
     print(jsonDecode(response.body));
     return Sensor.fromJson(jsonDecode(response.body)[0]);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load sensor data');
+  }
+}
+
+Future<List> getSensorTypes() async {
+  final response = await http.get(Uri.parse('https://andr3w.ddns.net/types'));
+
+  if (response.statusCode == 200) {
+    // parsing the data
+    var typesJson = jsonDecode(response.body)['types'];
+
+    // creating the list
+    List<String> types =
+        typesJson != null ? List.from(typesJson) : List.empty();
+
+    print(types);
+    // return the list
+    return types;
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
