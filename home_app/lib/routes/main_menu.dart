2834c886bd5e851e50b86a8410e5dc7e1473d474
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-04-30 16:54:23 +0300

    added: Card to display sensor data

--- home_app/lib/routes/main_menu.dart
+++ home_app/lib/routes/main_menu.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+// ignore: unused_import
 import '../models/sensor.dart';
+// ignore: unused_import
 import '../utils/dummy_fetch.dart';
+import '../widgets/custom_list_tile.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
@@ -12,13 +14,12 @@ class MainMenu extends StatefulWidget {
 }
 
 class _MainMenuState extends State<MainMenu> {
-  // late Future<Sensor> futureData;
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   futureData = fetchData();
-  // }
+  // to display sensors based on categories
+  Future<List> sensorTypes = getSensorTypes();
 
+  //
+  bool switchValue = false;
+  double sliderValue = 0;
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -33,50 +34,49 @@ class _MainMenuState extends State<MainMenu> {
             foregroundColor: Colors.grey,
             toolbarHeight: 30,
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Text(
-                  "Home",
-                  style: GoogleFonts.openSans(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+          body: ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            itemCount: 3,
+            itemBuilder: (context, index) => CustomListTile(
+              icon: Icons.home_rounded,
+              title: 'Ground floor',
+              subtitle: 'All lights on',
+              switchValue: switchValue,
+              onSwitchChange: (newSwitchValue) {
+                setState(() {
+                  switchValue = newSwitchValue;
+                });
+              },
+              sliderValue: sliderValue,
+              onSliderChange: (newSliderValue) {
+                setState(() {
+                  sliderValue = newSliderValue;
+                });
+              },
+              onTap: () {
+                // ignore: avoid_print
+                print('GO TO THE DETAILS PAGE');
+              },
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
               ),
-              Expanded(
-                child: ListView.separated(
-                  padding: const EdgeInsets.all(20),
-                  itemCount: 7,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: EdgeInsets.fromLTRB(10, 10, 50, 10),
-                      height: 100,
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                      child: Center(
-                        child: Text('Entry ${[index]}'),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(
-                    color: Colors.grey,
-                    height: 20,
-                    thickness: 1,
-                    indent: 40,
-                    endIndent: 40,
-                  ),
-                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
               ),
             ],
+            //currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: (parameter) {},
           ),
         ),
       ],
