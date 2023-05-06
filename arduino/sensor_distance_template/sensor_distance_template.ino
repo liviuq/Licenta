49082a49f354b02e03037c8de3b11f962d4c0489
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-05-06 17:56:48 +0300

    Updated the distance sensor code

--- arduino/sensor_distance_template/sensor_distance_template.ino
+++ arduino/sensor_distance_template/sensor_distance_template.ino
@@ -1,29 +1,30 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <RF24Network.h>
- 
 
-// RADIO MODULE
-RF24 radio(7, 8);  // nRF24L01(+) radio attached using Getting Started board
- 
-RF24Network network(radio);  // Network uses that radio
- 
-const uint16_t this_node = 01;   // Address of our node in Octal format
-const uint16_t base_station = 00;  // Address of the other node in Octal format
- 
-const unsigned long interval = 2000;  // How often (in ms) to send 'hello world' to the other unit
- 
-unsigned long last_sent;     // When did we last send?
-unsigned long packets_sent;  // How many have we sent already
- 
- 
+// radio module initialization
+RF24 radio(7, 8);
+
+// network initialization
+RF24Network network(radio);
+
+// constants that will be configured as per config_sensor.py
+const uint16_t this_node = 01; // octal format
+const uint16_t base_station = 00;
+
+// interval to send data
+const unsigned long interval = 2000;
+
+// time spent after last send
+unsigned long last_sent;
+unsigned long packets_sent;
+
+// defining the payload
 struct payload_t
-{  
-  // Structure of our payload
-  unsigned char type;
+{
+  unsigned char type[16];
   unsigned long value;
 };
- 
 
 // DISTANCE SENSOR
 const int trigPin = 4;
@@ -45,56 +46,57 @@ void calculate_distance()
   distance = duration * 0.034 / 2;
 }
 
-
 void setup(void)
 {
   Serial.begin(115200);
 
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-    
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+
   while (!Serial)
   {
     // some boards need this because of native USB capability
   }
- 
+
   if (!radio.begin())
   {
     Serial.println(F("Radio hardware not responding!"));
     while (1)
     {
-      // Flash in-built LED once per 500ms        
+      // Flash in-built LED once per 500ms
       digitalWrite(LED_BUILTIN, HIGH);
       delay(500);
       digitalWrite(LED_BUILTIN, LOW);
-      delay(500);           
+      delay(500);
     }
   }
 
   radio.setChannel(90);
-  network.begin(/*node address*/ this_node);
+  network.begin(this_node);
 }
- 
+
 void loop()
 {
-  network.update();  // Check the network regularly
- 
-  unsigned long now = millis(); // Timing the sending intervals
- 
-  // If it's time to send a message, send it!
-  if (now - last_sent >= interval) 
+  // polling the network for updates
+  network.update();
+
+  // time the messages properly
+  unsigned long now = millis();
+
+  // check if we have to send message
+  if (now - last_sent >= interval)
   {
-    //Calculating the distance
+    // Calculating the distance
     calculate_distance();
     Serial.print("Distance = ");
     Serial.println(distance);
-  
+
     last_sent = now;
- 
+
     Serial.print(F("Sending... "));
-    payload_t payload = { 'D', distance };
-    RF24NetworkHeader header(/*to node*/ base_station);
+    payload_t payload = {"Distance", distance};
+    RF24NetworkHeader header(base_station);
     bool ok = network.write(header, &payload, sizeof(payload));
-    Serial.println(ok ? F("ok.") : F("failed."));    
+    Serial.println(ok ? F("ok.") : F("failed."));
   }
 }
