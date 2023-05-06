f762183d1a9b9d968392f762ae4ecd1f5458de08
liviuq <petrache.andrei1@gmail.com>
2023-05-06 15:12:15 +0100

    Changed sensor type to long mode

--- flask/application/models.py
+++ flask/application/models.py
@@ -6,6 +6,6 @@ from . import db
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(8))
-    type = db.Column(db.String(1))
+    type = db.Column(db.String(8))
     value = db.Column(db.Integer)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
