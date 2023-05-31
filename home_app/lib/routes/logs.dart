bce27784605f682324e61b9ff011216a3902d59c
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-05-31 22:19:28 +0300

    Finished displaying only warning logs if left home and sensorTime is after leftHomeTime

--- home_app/lib/routes/logs.dart
+++ home_app/lib/routes/logs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:home_app/models/sensor.dart';
+import 'package:intl/intl.dart';
 
 import '../utils/fetch.dart';
 import '../widgets/sensor_data_tile.dart';
@@ -65,68 +66,124 @@ class _LogsRouteState extends State<LogsRoute> {
             } else {
               final sensorList = snapshot.data as List<Sensor>;
 
-              // get all the unique sensor addresses from sensorList
-              final List<String> uniqueAddresses = [];
-              for (final sensor in sensorList) {
-                if (!uniqueAddresses.contains(sensor.address)) {
-                  uniqueAddresses.add(sensor.address);
+              if (_secureMode) {
+// get all the unique sensor addresses from sensorList
+                final List<String> uniqueAddresses = [];
+                for (final sensor in sensorList) {
+                  if (!uniqueAddresses.contains(sensor.address)) {
+                    uniqueAddresses.add(sensor.address);
+                  }
                 }
-              }
 
-              // get all thresholds for each address
-              final List<double> thresholds = [];
-              for (final address in uniqueAddresses) {
-                thresholds.add(getThresholdFromDatabase(boxName: address));
-              }
+                // get all thresholds for each address
+                final List<double> thresholds = [];
+                for (final address in uniqueAddresses) {
+                  thresholds.add(getThresholdFromDatabase(boxName: address));
+                }
 
-              // create the map of address and threshold
-              Map<String, double> addressThresholdMap =
-                  Map.fromIterables(uniqueAddresses, thresholds);
+                // create the map of address and threshold
+                Map<String, double> addressThresholdMap =
+                    Map.fromIterables(uniqueAddresses, thresholds);
 
-              return ListView.builder(
-                itemCount: sensorList.length,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 3),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(4.0),
-                        decoration: BoxDecoration(
-                          // if the value is greater than the threshold, change the color
-                          color: (sensorList[index].value <=
-                                      addressThresholdMap[
-                                          sensorList[index].address]!) &&
-                                  _secureMode
-                              ? Colors.red
-                              : Colors.blue,
+                // create custom date format
+                DateFormat dateFormat =
+                    DateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'");
 
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Text(
-                          (index + 1).toString(),
-                          style: GoogleFonts.roboto(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                for (var sensor in sensorList) {
+                  if (sensor.value <= addressThresholdMap[sensor.address]! &&
+                      (dateFormat
+                              .parse(sensor.date)
+                              .add(const Duration(hours: 3)))
+                          .isAfter(_lastLeftHome)) {
+                    widgets.add(
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              // if the value is greater than the threshold, change the color
+                              color: Colors.red,
+
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Text(
+                              'WARN',
+                              style: GoogleFonts.roboto(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
+                          SensorDataTile(
+                            icon: Icons.broadcast_on_home_rounded,
+                            address: sensor.address,
+                            date: sensor.date,
+                            id: sensor.id,
+                            type: sensor.type,
+                            value: sensor.value,
+                            onTap: () => {},
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                }
+                if (widgets.isEmpty) {
+                  return Center(
+                    child: FloatingActionButton.extended(
+                      key: UniqueKey(),
+                      heroTag: UniqueKey(),
+                      label: Text(
+                        'No warnings',
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SensorDataTile(
-                        icon: Icons.broadcast_on_home_rounded,
-                        address: sensorList[index].address,
-                        date: sensorList[index].date,
-                        id: sensorList[index].id,
-                        type: sensorList[index].type,
-                        value: sensorList[index].value,
-                        onTap: () => {},
+                      backgroundColor: const Color(0xffe5e5e5),
+                      foregroundColor: const Color(0xff003049),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
                       ),
-                    ],
+                      onPressed: null,
+                    ),
                   );
-                },
-              );
+                } else {
+                  return ListView.builder(
+                    itemCount: widgets.length,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (BuildContext context, int index) {
+                      return widgets[index];
+                    },
+                  );
+                }
+              } else {
+                return Center(
+                  child: FloatingActionButton.extended(
+                    key: UniqueKey(),
+                    heroTag: UniqueKey(),
+                    label: Text(
+                      'Secure mode is off',
+                      style: GoogleFonts.roboto(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    backgroundColor: const Color(0xffe5e5e5),
+                    foregroundColor: const Color(0xff003049),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                    onPressed: null,
+                  ),
+                );
+              }
             }
           },
         ),
